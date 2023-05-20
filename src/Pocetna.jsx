ef29aca1ed5695b84372ff3b033f7761d831d43a
MafNik <marijana.nikacevic99@gmail.com>
2023-05-20 20:27:02 +0200

    dodata pocetna stranica

---
+++ src/Pocetna.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Pocetna() {
+    
+    return (
+        <div className="pocetna-stranica">
+            <div className="naslovna-slika">
+         
+            </div>
+            <div className="kratak-tekst">
+                <h2>Dobrodošli u našu veb prodavnicu slatkiša</h2>
+                <p>Uživajte u širokom izboru najkvalitetnijih slatkiša. Od čokolada do bombona, imamo sve što vam treba za zadovoljavanje vaših slatkih želja. Pogledajte naš asortiman i naručite svoje omiljene poslastice danas!</p>
+            </div>
+        </div>
+    );
+}
+
+export default Pocetna;
