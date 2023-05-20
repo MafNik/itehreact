caa16fa46fd8a2f7b77382c7eeec06a753d60524
MafNik <marijana.nikacevic99@gmail.com>
2023-05-20 20:35:23 +0200

    uvedeni osnovni koncepti rutiranja, kreirana komponenta navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import logo from './logo.svg';
+ 
 import './App.css';
 import Pocetna from './Pocetna';
 import Kontakt from './Kontakt';
-
+import Navbar from './Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-        <Pocetna></Pocetna>
-        <Kontakt></Kontakt>
-    </div>
+ 
+      
+        
+         
+        <BrowserRouter className="App">
+        <Navbar></Navbar>
+      <Routes>
+        <Route
+          path="/"
+          element={<Pocetna></Pocetna>}
+        />
+          
+         <Route path="/kontakt" element={<Kontakt></Kontakt>} />
+      </Routes>
+     
+  </BrowserRouter>
+  
   );
 }
 
