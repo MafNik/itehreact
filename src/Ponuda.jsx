d7b6c730b00bcb838b0786411877d351105b588b
MafNik <marijana.nikacevic99@gmail.com>
2023-05-20 20:53:02 +0200

    kreirani filteri za cenu

--- src/Ponuda.jsx
+++ src/Ponuda.jsx
@@ -1,12 +1,46 @@
-import React from 'react'; 
+import React, { useState } from 'react';
 import ProductCard from './ProductCard';
 
 function Ponuda({ products }) {
+  const [minPrice, setMinPrice] = useState('');
+  const [maxPrice, setMaxPrice] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState(products);
+
+  const handleFilter = () => {
+    const filtered = products.filter(
+      (proizvod) =>
+        proizvod.price >= parseInt(minPrice) && proizvod.price <= parseInt(maxPrice)
+    );
+    setFilteredProducts(filtered);
+  };
+
+  const handleReset = () => {
+    setMinPrice('');
+    setMaxPrice('');
+    setFilteredProducts(products);
+  };
+
   return (
     <div className="ponuda">
       <h2>Ponuda slatkiša</h2>
+      <div className="filter-container">
+        <input
+          type="number"
+          placeholder="Min cena"
+          value={minPrice}
+          onChange={(e) => setMinPrice(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Max cena"
+          value={maxPrice}
+          onChange={(e) => setMaxPrice(e.target.value)}
+        />
+        <button onClick={handleFilter}>Filtriraj</button>
+        <button onClick={handleReset}>Resetuj</button>
+      </div>
       <div className="kartice-container">
-        {products.map((proizvod) => (
+        {filteredProducts.map((proizvod) => (
           <ProductCard key={proizvod.id} proizvod={proizvod} />
         ))}
       </div>
