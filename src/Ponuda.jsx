7837632af71f198defc244f28075d9608f9d1fd9
MafNik <marijana.nikacevic99@gmail.com>
2023-05-20 21:12:46 +0200

    sortiranje po ceni rastuce ili opadajuce

--- src/Ponuda.jsx
+++ src/Ponuda.jsx
@@ -5,22 +5,31 @@ function Ponuda({ products }) {
   const [minPrice, setMinPrice] = useState('');
   const [maxPrice, setMaxPrice] = useState('');
   const [productName, setProductName] = useState('');
+  const [sortOrder, setSortOrder] = useState('asc');
   const [filteredProducts, setFilteredProducts] = useState(products);
 
   useEffect(() => {
-    const filtered = products.filter(
+    let filtered = products.filter(
       (proizvod) =>
         (proizvod.price >= parseInt(minPrice) || minPrice === '') &&
         (proizvod.price <= parseInt(maxPrice) || maxPrice === '') &&
         proizvod.name.toLowerCase().includes(productName.toLowerCase())
     );
+
+    if (sortOrder === 'asc') {
+      filtered.sort((a, b) => a.price - b.price);
+    } else if (sortOrder === 'desc') {
+      filtered.sort((a, b) => b.price - a.price);
+    }
+
     setFilteredProducts(filtered);
-  }, [minPrice, maxPrice, productName, products]);
+  }, [minPrice, maxPrice, productName, products, sortOrder]);
 
   const handleReset = () => {
     setMinPrice('');
     setMaxPrice('');
     setProductName('');
+    setSortOrder('asc');
   };
 
   return (
@@ -45,6 +54,13 @@ function Ponuda({ products }) {
           value={productName}
           onChange={(e) => setProductName(e.target.value)}
         />
+        <select
+          value={sortOrder}
+          onChange={(e) => setSortOrder(e.target.value)}
+        >
+          <option value="asc">Rastuća cena</option>
+          <option value="desc">Opadajuća cena</option>
+        </select>
         <button onClick={handleReset}>Resetuj</button>
       </div>
       <div className="kartice-container">
